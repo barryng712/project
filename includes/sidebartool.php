50d08a14077a7753fc2365a2abc030f78ca53215
dbinh712 <binhnguyen@trentu.ca>
2021-06-17 14:28:24 -0400

    initialize project

---
+++ includes/sidebartool.php
@@ -0,0 +1,19 @@
+<section>
+        <div>
+          <nav>
+            <ul>
+              <li><a href="main.html"><img src="https://img.icons8.com/ios-glyphs/100/000000/home-page.png"/></a><span>Home</span></li>
+              <li><a href="profile.html"><img src="https://img.icons8.com/ios-glyphs/100/000000/test-account.png"/></a><span>Profile</span></li>
+              <li><a href="sheets.html"><img src="https://img.icons8.com/wired/100/000000/add--v1.png"/></a><span>New Sheet</span></li>
+              <li><a href="history.html"><img src="https://img.icons8.com/dotty/100/000000/activity-history.png"/></a><span>History</span></li>
+              <li><a href=""><img src="https://img.icons8.com/android/100/000000/logout-rounded.png"/></a><span>Log out</span></li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <span class="search"><i class="fas fa-search"></i></span>
+          <input type="text" name="search" id="search">
+          <label for="go"><button type="submit" name="search" id="search" >Search</button></label>
+        </div>                  
+      </div>
+      </section>
